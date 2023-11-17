17a026f5b37df478bfd47c8e27815cf2f6c94acc
Victor Hugo Sousa <victorh@Victors-MacBook-Air.local>
2023-11-17 19:09:19 -0300

    adicionando expo push token

--- App.tsx
+++ App.tsx
@@ -1,3 +1,17 @@
+import React, { useEffect } from 'react'
+
+import registerNNPushToken, { getPushDataObject } from 'native-notify'
+
 import App from './src'
 
-export default App
+export default () => {
+  registerNNPushToken(15006, 'GjaYR5H3p888rbYvfB8srT')
+
+  const pushDataObject = getPushDataObject()
+
+  useEffect(() => {
+    console.log(pushDataObject)
+  }, [pushDataObject])
+
+  return (<App />)
+}
